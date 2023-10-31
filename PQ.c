d5dfef1a9e2426d1bd588f869210bb682ce52ae4
gabriel <101599265+wdw0@users.noreply.github.com>
2023-10-31 08:54:12 -0300

    Update PQ.c

--- PQ.c
+++ PQ.c
@@ -13,7 +13,7 @@ struct NO{
 
 struct priority_queue{
 
-    NO* queue;
+    NO* queue; //vertice e prioridade 
     int size; //tamanho atual
     int N;    //quant maxima de elementos
 
@@ -21,9 +21,24 @@ struct priority_queue{
 };
 
 
-//Funções ----------------------------------------------------------------------------
+//Função para criar o nó----------------------------------------------------------------------------------
+NO* inicia_no(int vertice, double prioridade){
+    //prioridade aqui seria a distancia
+
+    NO* no;
+    no->vertex = vertice;
+    no->priority = prioridade;
+
+}
+
+//Funções da fila de prioridade----------------------------------------------------------------------------
 
 PQ* PQ_create(int N){
+    PQ* pq = (PQ*)malloc(sizeof(PQ));
+
+    pq->queue = malloc(pq->N * sizeof(NO*));
+    pq->N = N + 1;
+    pq->size = 0;
 
 }
 
@@ -55,3 +70,21 @@ void PQ_destroy(PQ* pq){
 
 
 //TODO FIX UP E FIX DOWN
+
+void fix_down(PQ *pq, int k){
+    while (2*k <= pq->size) {
+        int j = 2*k;
+        if (j < pq->size && (pq->queue[j].priority > pq->queue[j+1].priority)){
+          j++;
+        }
+        if (pq->queue[k].priority < pq->queue[j].priority)) {
+            break;
+        }
+        
+        
+        //exch(a[k], a[j]);
+        //TODO trocar esse exch por uma função de swap 
+
+        k = j;
+    }
+}
